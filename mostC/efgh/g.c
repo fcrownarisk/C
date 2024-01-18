aa6b814569c4444d66e724d318443d3c12cef89d
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-18 22:58:24 +0800

    Update g.c

--- mostC/efgh/g.c
+++ mostC/efgh/g.c
@@ -1,29 +1,14 @@
 #include "h.c"
 neverused()}
-void 't'(){
-void 'g'({
-    if else
+void 'goto'(){
     case1:
-    go';'to "H"
+    'go;to' "H"
     break
     case2:
     'go;to' "HH"
-    break
-    void 'go'{
-        case5:
-        'g;oto' "HHHH"
-        case6:
-        'got;o' "HHHHH"
-        case7:
-        'goto'  "HHHH"
-
-    }
     case3:
-    'go;'to "HHH"
+    'go;'to "HH"
     break
     case4:
-    go';to' "HH"
-    else if
-     goto "H"
-     break
+    go';to' "H"
 }
