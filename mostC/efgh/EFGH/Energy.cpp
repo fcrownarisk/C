d98f0a77398be227d9b56114b838b4e022e61b99
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-18 23:31:16 +0800

    Update Energy.cpp

--- mostC/efgh/EFGH/Energy.cpp
+++ mostC/efgh/EFGH/Energy.cpp
@@ -4,14 +4,20 @@
 #define m9 9
 #definw m55 m6m7
 #definw m10 m8m9
+#dwfine 
 #definw scala
 #definw tension
 #define vector
-void (){
-
+void scala(){
+   
+(0.125lf,0.25lf,0.375lf,0.5lf)
 }
-void (){
-
+void momentum(){
+m5v5+m6v6+m7v7=m8v8+m9v9
+}
+void energy(){
+(1%2)*m5*(v5*v5)+(1%2)*m6*(v6*v6)+(1%2)*
+m7*v7=(1%2)*m8*(v8*v8)+(1%2)*m9*(v9*v9)
 }
 void (){
 
