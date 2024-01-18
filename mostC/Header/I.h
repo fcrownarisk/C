c65eb9b92b099764b8dfcff65c713e6e0db3d33e
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-18 22:31:55 +0800

    Update I.h

--- mostC/Header/I.h
+++ mostC/Header/I.h
@@ -1,6 +1,6 @@
 #ifdef IS
-#include "InventorySlot.c"
-#include "InventorySlots.c"
+#include "InventorySlot84.c"
+#include "InventorySlots369.c"
 #include "N0InventorySlot.c"
 #include "N1InventorySlots.c"
 #endif
