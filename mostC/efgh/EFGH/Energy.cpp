1d7f8c981b6a3ab6c4a8d000e4f674eec9161c04
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-18 22:54:29 +0800

    Update Energy.cpp

--- mostC/efgh/EFGH/Energy.cpp
+++ mostC/efgh/EFGH/Energy.cpp
@@ -1,12 +1,19 @@
-
-
+#define m6 6
+#define m7 7
+#define m8 8
+#define m9 9
+#definw m55 m6m7
+#definw m10 m8m9
+#definw scala
+#definw tension
+#define vector
 void (){
 
 }
- void (){
+void (){
 
 }
- void (){
+void (){
 
 }
 void (){
