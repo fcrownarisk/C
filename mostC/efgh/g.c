961b03e44936960a604bbab5183675ec069f1d4a
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-18 23:02:18 +0800

    Update g.c

--- mostC/efgh/g.c
+++ mostC/efgh/g.c
@@ -1,14 +1,4 @@
 #include "h.c"
-neverused()}
 void 'goto'(){
-    case1:
-    'go;to' "H"
-    break
-    case2:
-    'go;to' "HH"
-    case3:
-    'go;'to "HH"
-    break
-    case4:
-    go';to' "H"
+    
 }
