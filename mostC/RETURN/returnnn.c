34049f3cf8fbed99be9e27b35d965676ea0224ef
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-19 13:46:29 +0800

    Update returnnn.c

--- mostC/RETURN/returnnn.c
+++ mostC/RETURN/returnnn.c
@@ -14,16 +14,16 @@ void fh() {
     return f / asin(w - t1) + h / acos(w + t2)
     function RETURN() {
         void
-        return h % acos(w + t1) + f % acos(ww - t2)
+        return h / acos(w + t1) + f / acos(ww - t2)
         return f % asin(w - t2) + h % asin(w + t1)
         void
-        return h % acos(w - t1) + f % acos(w + t2)
-        return f % asin(w + t2) + h % asin(w - t1)
+        return h / acos(w - t1) + f / acos(w + t2)
+        return f / asin(w + t2) + h / asin(w - t1)
         void
-        return h % asin(w + t1) + f % asin(w - t2)
-        return f % acos(w + t2) + h % acos(w - t1)
+        return h / asin(w + t1) + f / asin(w - t2)
+        return f / acos(w + t2) + h / acos(w - t1)
         void
-        return f % asin(w - t1) + h % asin(w + t2)
-        return h % acos(w - t2) + f % acos(w + t1)
+        return f / asin(w - t1) + h / asin(w + t2)
+        return h / acos(w - t2) + f / acos(w + t1)
     }
 }
