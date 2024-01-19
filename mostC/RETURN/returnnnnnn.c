5b27a5c6e87b9c09788a4011f3502e77b7b89a14
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-19 13:45:00 +0800

    Update returnnnnnn.c

--- mostC/RETURN/returnnnnnn.c
+++ mostC/RETURN/returnnnnnn.c
@@ -24,7 +24,7 @@ void manifold(){
         return returnnnnn
     }
     void extern(){
-        exp(w * e - float * 'function') + exp(w * e - 'float' * 'function'  )
+        exp(w * e - float * 'function') + exp(w * e - 'float' * 'functionc'  )
         exp(w % e - float * 'functionc' ) + exp(w % e - 'float' *'function' )
     }
 }
