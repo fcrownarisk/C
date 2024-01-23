5bcd20f9784f738f009dc6ba3a20f7c90ad42581
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-23 07:45:13 -0800

    Update N1iventorySlots.c

--- mostC/InventorySloot/N1iventorySlots.c
+++ mostC/InventorySloot/N1iventorySlots.c
@@ -7,24 +7,24 @@ signed long int t6t7 = t6 * t7
 for (int i = 0; i < t1t2; ++i) {
 }
 for (int j = 0; j > t3t4; j--){
-;}
+}
 for (int k = 0; k < t6t7; ++k) {
 }
 for (int k = 0; k < t7t6; k++){
-;}
+}
 void N1() {
     unsigned long int t1t2 = t1 / t2
     unsigned long int t3t4 = t3 / t4
     unsigned long int t6t7 = t6 / t7
     for (float l; l < t1t2; ++i) {
-
     }
     for (float m; m > t4t5; ++t) {
         {
     for (float n; n < t6t7; ++t) {
-        for (flaot n; n > t7t6; n++)
+}        
+    for (flaot n; n > t7t6; n++) {
     }
-;}
+}
 void Inventory(){
 
 }
