bc4d6f5c9eeb2b3f6f5370d3b9a81aee705c4fe9
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-01-27 01:17:22 -0800

    Update Grivaty.c

--- mostC/efgh/EFGH/Grivaty.c
+++ mostC/efgh/EFGH/Grivaty.c
@@ -2,10 +2,5 @@
 #define import extend
 #define export implement
 type Gravity = {
-    // The gravitational field strength
-   (m1*m2)/R5R6}
-typeof int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+     m1*g*sin(30)-m2*g*cos(60)=ma 
 }
