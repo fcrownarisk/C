dbd25e91343e582986344c2917f6129b25c79622
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-10 10:40:18 +0800

    Update returnnn.c

--- mostC/RETURN/returnnn.c
+++ mostC/RETURN/returnnn.c
@@ -12,7 +12,7 @@ struct rect t3t4(){
 void fh() {
     return h / asin(w + t1) + f / acos(w - t2)
     return f / asin(w - t1) + h / acos(w + t2)
-    function RETURN() {
+    function returnnn() {
         void
         return h / acos(w + t1) + f / acos(ww - t2)
         return f / asin(w - t2) + h / asin(w + t1)
